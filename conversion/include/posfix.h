790ba332c7233da0f4d029b943fa0a5957d89b2f
softwarejimenez <softwarejimenez@gmail.com>
2014-04-21 19:34:49 +0200

    V0.9.4 posfix

--- conversion/include/posfix.h
+++ conversion/include/posfix.h
@@ -8,29 +8,21 @@ Fermath Project:Posfix
 Version:0.9.4
 */
 
-#include<stdio.h>
-#include <string>
-#include <vector>
-#include <set>
-#include <iostream>
-#include <stack>
-#include <fstream>
 #include "expression.h"
-
 
 
 using namespace std;
 
 //devuelve true si el operador, dado a partir del string, esta en el conjunto
-bool operador(const string &a,const op_list &b) {
-    return b.find_operator(a);
+bool operador(const string &a,const data_src &datasrc) {
+    return datasrc.is_operator(a);
 }
 
 //comprueba si en el string a se encuentra un operador o un parentesis. Devuelve la posicion en la que empieza.
-int comprobaroperador(const string &a,const op_list &b) {
+int comprobaroperador(const string &a,const data_src &datasrc) {
     int pos;
     set<op>::iterator it;
-    for(it=b.begin(); it!=b.end(); ++it) {//recorre el conjunto de operadores comprobandolo.
+    for(it=datasrc.op_begin(); it!=datasrc.op_end(); ++it) {//recorre el conjunto de operadores comprobandolo.
         if((pos=a.find((*it).get_text()))!=-1) return pos;
     }
     if((pos=a.find(")"))!=-1) return pos;
@@ -51,16 +43,9 @@ void mostrar(const vector<string> &a) {
     cout<<endl;
 }
 
-//crea el conjunto de operadores a partir de un archivo binario.
-op_list cargarop_list(ifstream &input) {
-    op_list v;
-    v.read_setoperator(input);
-    return v;
-}
-
 //Tiene de entrada el conjunto de operandos y un string con la operacion completa. Devuelve un vector de string.
 //Donde cada string sera una variable, un operador o un string aux(#)
-vector<string> convertir(const string &infijo,const op_list &vop) {  //convierte en un vector de string
+vector<string> convertir(const string &infijo,const data_src &datasrc) {   //convierte en un vector de string
     int tama=infijo.size();
     string aux("#");//Es un string auxliaiar que se coloca delante de los operadores para distinguirlos
     vector< string > salida;
@@ -76,11 +61,11 @@ vector<string> convertir(const string &infijo,const op_list &vop) {  //convierte
         else {//sin o es un parentesis
             b.push_back(infijo[i]);
             i++;
-            while((pos=comprobaroperador(b,vop))==-1 && i<tama) {//mientras no sea un operador, va rellenando el string b.
+            while((pos=comprobaroperador(b,datasrc))==-1 && i<tama) {//mientras no sea un operador, va rellenando el string b.
                 b.push_back(infijo[i]);
                 ++i;
             }
-            if(operador(b,vop)) { //si es un operador, que metar # y luego el operador
+            if(operador(b,datasrc)) { //si es un operador, que metar # y luego el operador
                 salida.push_back(aux);
                 salida.push_back(b);
             }
@@ -103,15 +88,17 @@ vector<string> convertir(const string &infijo,const op_list &vop) {  //convierte
 //calcula la priodioriadad. compara el valor del tope de la pila y el valor del string del infijo actual.
 //devuelve 0 si pila tiene mas prioridad que infijo y devuevle 1 si al pila tiene menos prioridad que infijo
 //la prioridad va de unary */ -+
-int fprioridad(const string &pila,const string &infijo,const op_list &vop) {    //tener en cuenta ^
+int fprioridad(const string &pila,const string &infijo,const data_src &datasrc) {     //tener en cuenta ^
+    op oppila=datasrc.get_operator(pila);
+    op opinfijo=datasrc.get_operator(infijo);
     int p=1;
-    int idPilatope=vop.get_ide(pila);//calculamso el identificador del operador de pila[tope]
-    int idinfijoci=vop.get_ide(infijo);//calculamso el identificador del operador deinfijo[ci]
-    if(idPilatope==-1) {//si en al pila encontramos un operador unario este tiene siempre preferencia.
+    if(oppila.is_unary()) { //si en al pila encontramos un operador unario este tiene siempre preferencia.
         p=0;
     }
     else {
-        if(idPilatope!=-1 && idinfijoci!=-1) { //si ambos operadores son binarios
+        if(oppila.is_binary() && opinfijo.is_binary()) { //si ambos operadores son binarios
+            int idPilatope=oppila.get_id();//calculamso el identificador del operador de pila[tope]
+            int idinfijoci=opinfijo.get_id();//calculamso el identificador del operador deinfijo[ci]
             if(idPilatope==5 && idinfijoci<=5) 	p=0;//id=5 -> ^
             else {
                 if((idPilatope==4 || idPilatope==3) && (idinfijoci<=4 || idinfijoci<=3)) p=0; //id=4 -> /. id=3 -> *
@@ -134,7 +121,7 @@ int fprioridad(const string &pila,const string &infijo,const op_list &vop) {    
 //-Las diferentes pilas
 //-un vector de string en infijo
 //-El conjunto de operadores.
-vector<string> pasarpostfijo(const vector<string> &infijo,const op_list &vop,stack<int> &posiciones01,stack<string> &variables,stack<op> &operadores) {
+vector<string> pasarpostfijo(const vector<string> &infijo,const data_src &datasrc,stack<int> &posiciones01,stack<string> &variables,stack<op> &operadores) {
     int ci,tope,cp;
     cp=ci=tope=0;
     //cp contadorpostfijo
@@ -152,7 +139,7 @@ vector<string> pasarpostfijo(const vector<string> &infijo,const op_list &vop,sta
         else {
             if(")"==infijo[ci]) {//si es un parentesis cerrado.
                 while(Pila[tope]!="(") {//mientras no aparezca un parentesis abierto metemos los operadores en la pila de operadores y en el vector de postfijo
-                    operadores.push(vop.get_op(Pila[tope]));//cargamos en la pila de operadores
+                    operadores.push(datasrc.get_operator(Pila[tope]));//cargamos en la pila de operadores
                     posiciones01.push(1);//metemos un 1 para determinar que es operador
                     Postfijo[cp]=Pila[tope];//saca de la pila los operadores necesarios; y los mete en posfijo.
                     cp++;
@@ -173,9 +160,9 @@ vector<string> pasarpostfijo(const vector<string> &infijo,const op_list &vop,sta
                     if(Pila[tope]!="(") {//si en la en el tope de la pila no hay un parentesis abierto comprobamos la propiedad
                         // prioridad=1;//si prioridad=1 introduce en el Postfijo el valor actual de infijo. Si prioridad=0 introduce el valor que habia en la Pila.
                         //comprueba la prioridad
-                        if(!fprioridad(Pila[tope],infijo[ci],vop)) {
+                        if(!fprioridad(Pila[tope],infijo[ci],datasrc)) {
                             while(tope>0 && Pila[tope]!="(") {//cargamos los operadores
-                                operadores.push(vop.get_op(Pila[tope]));//en la pila de operadores
+                                operadores.push(datasrc.get_operator(Pila[tope]));//en la pila de operadores
                                 posiciones01.push(1);//metemos un 1 para determinar que es operador
                                 Postfijo[cp]=Pila[tope];//escribimos en el Postfijo el operador de la Pila
                                 cp++;
@@ -190,7 +177,7 @@ vector<string> pasarpostfijo(const vector<string> &infijo,const op_list &vop,sta
         }
     }
     while(tope>0) {//si quedan operadores en la pila, los metemos en:
-        operadores.push(vop.get_op(Pila[tope]));//la pila de operadores
+        operadores.push(datasrc.get_operator(Pila[tope]));//la pila de operadores
         posiciones01.push(1);//metemos un 1 para determinar que es operador
         Postfijo[cp]=Pila[tope];//el vector de postfijo
         cp++;
@@ -203,37 +190,14 @@ vector<string> pasarpostfijo(const vector<string> &infijo,const op_list &vop,sta
     //-una pila de int para determinar si va 0=variable y 1=operador
 }
 
-
-
-/*main() {
-//creamos la base de datos de operadores
-    ifstream input("op.fermath");
-    op_list vop=cargarop_list(input);
-//aconsejable eliminar espacios y comprobar parentesis
-//modificar los corchetes por ()
-    string a ("((cos(b)-1kg)*sen(5)-(1T*5m))");//funciona tanto si ponemos sen(5) como sen5
+vector <string> convertirpostfijo(const string &a, const data_src &datasrc) {
     vector< string > infijo,Postfijo;
-    infijo=convertir(a,vop);
-    cout<<"mostramos en infijo"<<endl;
-    mostrar(infijo);
-
-
-    //creamos 3 pilas:
-    //-una pila de string con las variables
-    //-una pila de operator con los operadores
-    //-una pila de int para determinar si va 0=variable y 1=operador
+    infijo=convertir(a,datasrc);
     stack<int> posiciones01;
     stack<string> variables;
     stack<op> operadores;
-    Postfijo=pasarpostfijo(infijo,vop,posiciones01,variables,operadores);
+    Postfijo=pasarpostfijo(infijo,datasrc,posiciones01,variables,operadores);
     cout<<"mostramos el vector de string en postfijo"<<endl;
     mostrar(Postfijo);
-    cout<<"mostramos la pila de las variables"<<endl;
-    show_stack(variables);
-    cout<<"mostramos la pila de los operadores"<<endl;
-    show_stack(operadores);
-    cout<<"mostramos la pila de las posiciones01 0=variable y 1=operador "<<endl;
-    show_stack(posiciones01);
-    cout<<endl<<endl;
-    return 0;
-}*/
+    return Postfijo;
+}
---
+++ conversion/posfijo_test.cpp
@@ -0,0 +1,38 @@
+/* (C) Programmed by:
+   Antonio Jimenez Martínez
+   Andrés Ortiz Corrales
+   Mariano Palomo Villafranca  */
+/*
+Fermath Project:Posfix Test
+Version:0.9.4
+
+This program test units,magnitudes,operators and glossary
+*/
+
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <set>
+#include <iostream>
+#include <stack>
+#include <fstream>
+#include <cmath>
+#include <cstdlib>
+#include <map>
+using namespace std;
+#include "include/posfix.h"
+
+int main() {
+    cout<<"Fermath posfijo Test "<<endl;
+    data_src database;
+    const basic_unit_source *bsrc;
+    bsrc=database.get_basic_source2();
+    ifstream input("database.fermath");
+    database.read(input);
+    input.close();
+    vector< string > Postfijo;
+    string infijo("((cos(b)-1kg)*sen(5)-(1T*5m))");
+    Postfijo=convertirpostfijo(infijo,database);
+    cout<<endl;
+    return 0;
+}
